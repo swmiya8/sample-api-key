80cae904a23a15a5b9945dbefe063357f424598e
sow <157239119+swmiya8@users.noreply.github.com>
2026-03-12 14:13:15 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const app = express();
+//trigger
+app.get("/users", (req, res) => {
+  res.json({ users: [] });
+});
+//trigger
+app.post("/users", (req, res) => {
+  res.json({ message: "User created" });
+});
+//trigger
+app.put("/users/:id", (req, res) => {
+  res.json({ message: "User updated" });
+});
 
+app.delete("/users/:id", (req, res) => {
+  res.json({ message: "User deleted" });
+});
+//trigger
+module.exports = app;
